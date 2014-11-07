79c92a9d4e64aca16500033e174753b600b5bca9
Emil <emil.bergold@outlook.com>
2014-11-07 23:55:55 +0100

    Update init

--- bin/init.php
+++ bin/init.php
@@ -3,16 +3,4 @@ require_once 'lib/storage.php';
 
 $sh = Storage::fromDefaultBucket();
 
-$options = [
-  'gs' => [
-        'Content-Type' => 'text/plain'
-  ],
-];
-$ctx = stream_context_create($options);
-file_put_contents($sh->buildPath('logs/ricochetrobots.log'), '', 0, $ctx);
-
-function stry($msg, $res) {
-    echo $msg;
-    echo $res ? " succeeded" : " failed";
-    echo "\n";
-}
+$sh->fileWrite('logs/iniii.log', 'hejehe');
