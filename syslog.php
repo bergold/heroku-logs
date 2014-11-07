050f33c923e42a64771d78140822f4870daf7667
Emil <emil.bergold@outlook.com>
2014-11-07 20:05:08 +0100

    Update syslog.php

--- syslog.php
+++ syslog.php
@@ -1,8 +1,17 @@
 <?php
+$path_prefix = '\/syslog\/';
+
 $data = '';
-$header = getallheaders();
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$m = preg_match("/(?<=^$path_prefix)[a-z-]*$/", $path, $m);
+if (!$m || $m == 0) {
+  syslog(LOG_WARNING, "Invalid requestformat: $path");
+  http_response_code(400);
+  exit(1);
+}
+$header = http_get_request_headers();
   array_walk($final, function($val, $key) use(&$data){
-	$data .= "$val: $key;";
+  	$data .= "$val: $key;";
 });
 $data .= @file_get_contents('php://input');
 
