4708755b1afff52e907f669f6a4051320384e43a
Emil <emil.bergold@outlook.com>
2014-11-09 01:52:10 +0100

    error handling

--- bin/loghandler.php
+++ bin/loghandler.php
@@ -8,9 +8,9 @@ $drain_token = $_SERVER["HTTP_LOGPLEX_DRAIN_TOKEN"];
 $msg_count = $_SERVER["HTTP_LOGPLEX_MSG_COUNT"];
 
 if ($app_name === false) {
-  syslog(LOG_WARNING, "Invalid request format: $path");
-  http_response_code(400);
-  exit(1);
+    syslog(LOG_WARNING, "Invalid request format: $path.");
+    http_response_code(400);
+    exit(1);
 }
 
 $data = @file_get_contents("php://input");
@@ -20,11 +20,27 @@ $storage_handle = Storage::fromDefaultBucket("/logs/");
 Logger::setStorageInstance($storage_handle);
 
 $logger = Logger::get($app_name);
-$logger->validateDrain($drain_token);
+if ($logger === false) {
+    syslog(LOG_WARNING, "For the name '$app_name' is no logger registered.");
+    http_response_code(404);
+    exit(1);
+}
 
-$logger->append($data);
+$valid = $logger->validateDrain($drain_token);
+if (!$valid) {
+    syslog(LOG_WARNING, "$drain_token is no valid token for $app_name.");
+    http_response_code(403);
+    exit(1);
+}
 
-syslog(LOG_INFO, "Got $msg_count log" . ($msg_count == 1 ? "" : "s") . " from $app_name ($drain_token)");
+$succeed = $logger->append($data);
+if (!$succeed) {
+    syslog(LOG_ERR, "Appending the data to the logs failed.");
+    http_response_code(500);
+    exit(2);
+}
+
+syslog(LOG_INFO, "Got $msg_count log" . ($msg_count == 1 ? "" : "s") . " from $app_name ($drain_token).");
 
 http_response_code(204);
 exit(0);
