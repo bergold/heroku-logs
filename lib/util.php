738452571881871dc8258edbd5fb7e77c4fa2ab7
Emil <emil.bergold@outlook.com>
2014-11-09 18:11:20 +0100

    path function

--- lib/util.php
+++ lib/util.php
@@ -1,7 +1,15 @@
 <?php
 
-function get_appname($prefix = "\/syslog\/") {
+function path($prefix = "") {
+    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    if ($prefix == "") return $path;
+    $regex = "/^" . preg_quote($prefix, "/") . "/";
+    return preg_replace($regex, "", $path);
+}
+
+function get_appname($prefix = "/syslog/") {
     $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $prefix = preg_quote($prefix, "/");
     $m = preg_match("/(?<=^$prefix)[a-z-]*$/", $path, $match);
     if (!$m || $m == 0) return false;
     return $match[0];
